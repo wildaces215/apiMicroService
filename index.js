247ecdd703821487fad6a9a8ab2a8b0ee2df53c0
Craig <wannabecoastie@gmail.com>
2017-11-17 11:20:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+//Declared packages required 
 var express=require('express')
 var app=express();
 var path=require('path');
 var fs =require('fs');
 var moment=require('moment');
-
+//Gets directory for views
 app.use(express.static(__dirname + '/views'));
 
+
+//Sets the port to be used for production
 app.set('port', (process.env.PORT || 8080));
 app.get("/",function(req,res){
    res.sendFile('index.html');
 })
-
+//gets the input user enters into the url
 app.get('/:input',function(req,res){
  var userInputUrl;
 var unixOutput=null;
@@ -23,7 +26,8 @@ if(req.params.input>=0){
   naturalOutput=moment.unix(req.params.input).format("MMMM D, YYYY");
 }
 else if(isNaN(req.params.input)&& moment(req.params.input, "MMMM D, YYYY").isValid()){
-  userInputUrl=moment(req.params.input, "MMMM D, YYYY").format("X");
+  //use moment js to modiy url the user put in 
+   userInputUrl=moment(req.params.input, "MMMM D, YYYY").format("X");
   unixOutput=userInputUrl;
   naturalOutput=moment.unix(userInputUrl).format("MMMM D, YYYY");
 }
@@ -35,6 +39,7 @@ res.json({
 
 
 })
+//Launch app at specifed port
 app.listen(app.get('port'),function(req,res){
   console.log("App running on"+app.get('port'));
 })
